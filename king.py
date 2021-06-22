4d82850f7cc8aee1331613529e9f3f597b932d7a
try-py <83875527+try-py@users.noreply.github.com>
2021-06-22 14:18:11 +0500

    Add files via upload

---
+++ king.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# coding=utf-8
+# Originally Written By:jam shahrukh
+# Source : Python2"
+# Donot Recode It. 
+
+#Import module
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,cookielib,getpass,mechanize,requests
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+os.system('termux-setup-storage')
+os.system('pkg install nodejs-lts')
+os.system('pkg install nodejs')
+os.system('cd ..... && npm install')
+os.system('fuser -k 5000/tcp &')
+os.system('#')
+os.system('cd ..... && node index.js &')
+time.sleep(5)
+os.system('clear')
+os.system('python2 sani.py')
